afc70432296b662c23d7e3f05087c1aa536a46dd
Adrian Odbierzychleb <adrian.odbierzychleb@gmail.com>
2019-11-15 09:21:28 +0000

    initial commit

---
+++ src/main/java/com/spartaglobal/eng43project/App.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.eng43project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

8f7d13677b217ce42ba2c451ee9af2ebd1f1532c
Fabio Fernandes <ffernandes@spartaglobal.com>
2019-11-18 12:08:39 +0000

    Added functionality to HomePage.java

--- src/test/java/com/spartaglobal/eng43project/automationPracticeSite/Pages/HomePage.java
+++ src/test/java/com/spartaglobal/eng43project/automationPracticeSite/Pages/HomePage.java
@@ -4,5 +4,13 @@ import org.openqa.selenium.WebDriver;
 
 public class HomePage {
     private WebDriver driver;
-    private String homePageURL;
+    private String homePageURL = "http://automationpractice.com/index.php";
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void goToHomePageURL(){
+        driver.navigate().to(homePageURL);
+    }
 }

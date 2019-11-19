db6c2f1f674856ea4eca5b88266c0d4b6e293b43
Anis Subba <21302329@student.uwl.ac.uk>
2019-11-19 16:39:03 +0000

    Changed functionality of Basket and BasketTest

--- src/test/java/com/spartaglobal/eng43project/automationPracticeSite/Pages/Basket.java
+++ src/test/java/com/spartaglobal/eng43project/automationPracticeSite/Pages/Basket.java
@@ -1,15 +1,13 @@
 package com.spartaglobal.eng43project.automationPracticeSite.Pages;
 
 import com.spartaglobal.eng43project.automationPracticeSite.Pages.Navigation.NavigationPages;
+import io.cucumber.java.bs.A;
+import io.cucumber.java.en_old.Ac;
 import org.openqa.selenium.By;
-<<<<<<< HEAD
-import org.openqa.selenium.Keys;
-=======
->>>>>>> basket
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
-import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,72 +15,42 @@ public class Basket {
     private WebDriver driver;
     private NavigationPages navigationPages;
     private String basketUrl = "http://automationpractice.com/index.php?controller=order";
-<<<<<<< HEAD
-    private By cloth2AddToBasketID = By.cssSelector("#center_column > ul > li:nth-child(2) > div > div.right-block > div.button-container > a.button.ajax_add_to_cart_button.btn.btn-default");
-    private By cloth3AddToBasketID = By.cssSelector("#center_column > ul > li.ajax_block_product.col-xs-12.col-sm-6.col-md-4.last-in-line.first-item-of-tablet-line.last-item-of-mobile-line > div > div.right-block > div.button-container > a.button.ajax_add_to_cart_button.btn.btn-default");
     private By checkoutButtonID = By.linkText("Proceed to checkout");
     private By continueToShoppingID = By.cssSelector("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > span");
-=======
-
-    private NavigationPages navigationPages;
     private By printedDressQuickBuy = By.cssSelector("#homefeatured > li.ajax_block_product.col-xs-12.col-sm-4.col-md-3.last-item-of-tablet-line.first-item-of-mobile-line > div > div.right-block > div.button-container > a.button.ajax_add_to_cart_button.btn.btn-default > span");
-    private By proceedToCheckoutButton = By.cssSelector("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a > span");
-
->>>>>>> basket
 
     public Basket(WebDriver driver) {
         this.driver = driver;
         navigationPages = new NavigationPages(driver);
     }
 
-<<<<<<< HEAD
-
-    public Basket goToBasketURL(){
-=======
     public Basket goToBasketURL() {
->>>>>>> basket
         driver.navigate().to(basketUrl);
         return this;
     }
 
-<<<<<<< HEAD
     public Basket goToWomanPage(){
         navigationPages.womensPage().goToWomensPage();
         return this;
     }
 
-    public Basket selectProduct1ToCart(){
-        goToWomanPage();
-        driver.findElement(cloth3AddToBasketID).click();
-        return this;
-    }
 
     public Basket clickShoppingButton(){
-        selectProduct1ToCart().waitingTime();
+        waitForElement();
         driver.findElement(continueToShoppingID).click();
         return this;
     }
 
-    public Basket selectProduct2ToCart(){
-        clickShoppingButton();
-        driver.findElement(cloth2AddToBasketID).click();
-        return this;
-    }
-
-    public Basket waitingTime(){
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        return this;
-    }
-
-    public Basket processToCheckout(){
-        selectProduct2ToCart().waitingTime();
+    public Basket processToCheckout() {
+        waitForElement();
         driver.findElement(checkoutButtonID).click();
-=======
+        return this;
+    }
+
     public Basket goToHomePageURL(){
         navigationPages.homePage().goToHomePageURL();
         return this;
     }
-
 
     public Basket waitForElement(){
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -98,8 +66,23 @@ public class Basket {
     public Basket proceedToCheckout(){
         addItemToBasket().waitForElement();
         driver.findElement(By.linkText("Proceed to checkout")).click();
->>>>>>> basket
         return this;
     }
 
+    public Basket selectMultipleProductToCart(){
+        WebElement from;
+        goToWomanPage();
+        Actions action = new Actions(driver);
+        for (int i = 1; i < 8; i++ ){
+            from = driver.findElement(By.cssSelector("#center_column > ul > li:nth-child("+i+") > div > div.right-block > div.button-container > a.button.ajax_add_to_cart_button.btn.btn-default"));
+            from.click();
+            if( i == 7 ){
+                processToCheckout();
+            }
+            else {
+                clickShoppingButton();
+            }
+        }
+        return this;
+    }
 }

683e0ccef6ec260143489898fb672cd5546696b3
NGeorgiev <NGeorgiev@spartaglobal.com>
2019-11-19 16:13:05 +0000

    added an input test

--- src/test/java/com/spartaglobal/eng43project/ProjectTests/BasketTests/BasketTest.java
+++ src/test/java/com/spartaglobal/eng43project/ProjectTests/BasketTests/BasketTest.java
@@ -3,29 +3,41 @@ package com.spartaglobal.eng43project.ProjectTests.BasketTests;
 import com.spartaglobal.eng43project.SeleniumConfig.SeleniumConfig;
 import com.spartaglobal.eng43project.automationPracticeSite.AutomationPracticeSite;
 import com.spartaglobal.eng43project.automationPracticeSite.Pages.Basket;
+import com.spartaglobal.eng43project.automationPracticeSite.Pages.Navigation.NavigationPages;
 import io.cucumber.java.eo.Se;
 import org.junit.*;
+import org.openqa.selenium.By;
 
 public class BasketTest {
 
     private static SeleniumConfig seleniumConfig;
     private static AutomationPracticeSite automationPracticeSite;
+    private static NavigationPages navigationPages;
 
     @BeforeClass
-    public static void setup(){
+    public static void setup() {
         // TODO: 18/11/2019 Change the driverPath to your own path!
-        seleniumConfig = new SeleniumConfig("chrome","C:\\Users\\IYotova\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        seleniumConfig = new SeleniumConfig("chrome", "C:\\Users\\NGeorgiev\\Downloads\\chromedriver_win32\\chromedriver.exe");
         automationPracticeSite = new AutomationPracticeSite(seleniumConfig.getDriver());
+        navigationPages = new NavigationPages(seleniumConfig.getDriver());
     }
 
-    @AfterClass
-    public static void teardown(){
-        seleniumConfig.getDriver().close();
-    }
+//    @AfterClass
+//    public static void teardown(){
+//        seleniumConfig.getDriver().close();
+//    }
+
+//    @Test
+//    public void checkUrlIsOpened(){
+//        automationPracticeSite.getBasket().goToBasketURL();
+//        Assert.assertEquals("http://automationpractice.com/index.php?controller=order",seleniumConfig.getDriver().getCurrentUrl());
+//    }
 
     @Test
-    public void checkUrlIsOpened(){
-        automationPracticeSite.getBasket().goToBasketURL();
-        Assert.assertEquals("http://automationpractice.com/index.php?controller=order",seleniumConfig.getDriver().getCurrentUrl());
+    public void goToDressSite() {
+        automationPracticeSite.getBasket().goToProceedToCheckout().getQuantity("00");
+
+
     }
 }
+
--- src/test/java/com/spartaglobal/eng43project/automationPracticeSite/Pages/Basket.java
+++ src/test/java/com/spartaglobal/eng43project/automationPracticeSite/Pages/Basket.java
@@ -1,16 +1,48 @@
 package com.spartaglobal.eng43project.automationPracticeSite.Pages;
 
+import com.spartaglobal.eng43project.automationPracticeSite.Pages.Navigation.DressesPage;
+import com.spartaglobal.eng43project.automationPracticeSite.Pages.Navigation.NavigationPages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Basket {
     private WebDriver driver;
+    private NavigationPages navigationPages;
+    private String dressesPageURL = "http://automationpractice.com/index.php?id_category=8&controller=category";
     private String basketUrl = "http://automationpractice.com/index.php?controller=order";
+
     public Basket(WebDriver driver) {
         this.driver = driver;
     }
 
     public Basket goToBasketURL(){
         driver.navigate().to(basketUrl);
+        return this;
+    }
+    public Basket goToDressesPage(){
+        driver.navigate().to(dressesPageURL);
+        return this;
+    }
+    public Basket SelectItemAndAddToCart(){
+        goToDressesPage();
+        driver.findElement(By.cssSelector("#center_column > ul > li:nth-child(1) > div > div.right-block > div.button-container > a.button.ajax_add_to_cart_button.btn.btn-default")).click();
+        return this;
+    }
+    public Basket waitForElement(){
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return this;
+    }
+    public Basket goToProceedToCheckout(){
+        SelectItemAndAddToCart().waitForElement();
+        driver.findElement(By.linkText("Proceed to checkout")).click();
+        return this;
+    }
+    public Basket getQuantity(String quantity){
+        driver.findElement(By.name("quantity_3_13_0_0")).sendKeys(quantity);
+        driver.findElement(By.name("quantity_3_13_0_0")).sendKeys(Keys.ENTER);
         return this;
     }
 }

4f40de9c3d17c0fbba54f4c8efcd2f6318f12bb2
IYotova <iliyanayotova@bennington.edu>
2019-11-19 16:01:49 +0000

    removed unnecessary class in home

--- src/test/java/com/spartaglobal/eng43project/automationPracticeSite/Pages/Navigation/HomePage.java
+++ src/test/java/com/spartaglobal/eng43project/automationPracticeSite/Pages/Navigation/HomePage.java
@@ -6,11 +6,9 @@ import org.openqa.selenium.WebDriver;
 public class HomePage {
     private String homePageURL = "http://automationpractice.com/index.php";
     private WebDriver driver;
-    private CommonPageProperties commonPageProperties;
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
-        this.commonPageProperties = new CommonPageProperties(driver);
     }
 
     public HomePage goToHomePageURL(){

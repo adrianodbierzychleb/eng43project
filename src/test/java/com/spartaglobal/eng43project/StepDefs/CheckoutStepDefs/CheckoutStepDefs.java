0eddae1d1c2e4db440c0d9e05eb08ad149745e03
Bailee Beckles <bbeckles@spartaglobal.com>
2019-11-19 16:10:06 +0000

    added changes

--- src/test/java/com/spartaglobal/eng43project/StepDefs/CheckoutStepDefs/CheckoutStepDefs.java
+++ src/test/java/com/spartaglobal/eng43project/StepDefs/CheckoutStepDefs/CheckoutStepDefs.java
@@ -9,7 +9,7 @@ import io.cucumber.java.en.When;
 
 public class CheckoutStepDefs {
     // TODO: 18/11/2019  Change your driver path to your own path!
-    private SeleniumConfig seleniumConfig = new SeleniumConfig("chrome","C:\\Users\\Fabio Fernandes\\Downloads\\chromedriver_win32\\chromedriver.exe");
+    private SeleniumConfig seleniumConfig = new SeleniumConfig("chrome","C:\\BB\\chromedriver_win32\\chromedriver.exe");
     private AutomationPracticeSite automationPracticeSite = new AutomationPracticeSite(seleniumConfig.getDriver());
 
     @Given("I have an account")

a130d122b746fe915775957d2ed249d3760bde6f
Anis Subba <21302329@student.uwl.ac.uk>
2019-11-19 15:05:17 +0000

    added method that adds 2 product to checkout

--- src/test/java/com/spartaglobal/eng43project/ProjectTests/BasketTests/BasketTest.java
+++ src/test/java/com/spartaglobal/eng43project/ProjectTests/BasketTests/BasketTest.java
@@ -3,29 +3,53 @@ package com.spartaglobal.eng43project.ProjectTests.BasketTests;
 import com.spartaglobal.eng43project.SeleniumConfig.SeleniumConfig;
 import com.spartaglobal.eng43project.automationPracticeSite.AutomationPracticeSite;
 import com.spartaglobal.eng43project.automationPracticeSite.Pages.Basket;
+import com.spartaglobal.eng43project.automationPracticeSite.Pages.Navigation.NavigationPages;
+import io.cucumber.java.en_old.Ac;
 import io.cucumber.java.eo.Se;
 import org.junit.*;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasketTest {
 
     private static SeleniumConfig seleniumConfig;
     private static AutomationPracticeSite automationPracticeSite;
+    private static NavigationPages navigationPages;
 
     @BeforeClass
     public static void setup(){
         // TODO: 18/11/2019 Change the driverPath to your own path!
-        seleniumConfig = new SeleniumConfig("chrome","C:\\Users\\IYotova\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        seleniumConfig = new SeleniumConfig("chrome","C:\\Users\\Anis Subba\\Downloads\\chromedriver_win32\\chromedriver.exe");
         automationPracticeSite = new AutomationPracticeSite(seleniumConfig.getDriver());
+        navigationPages = new NavigationPages(seleniumConfig.getDriver());
     }
 
-    @AfterClass
+    /*@AfterClass
     public static void teardown(){
         seleniumConfig.getDriver().close();
-    }
+    }*/
 
     @Test
     public void checkUrlIsOpened(){
         automationPracticeSite.getBasket().goToBasketURL();
         Assert.assertEquals("http://automationpractice.com/index.php?controller=order",seleniumConfig.getDriver().getCurrentUrl());
+    }
+
+    /*Given I am in Woman Catalog page
+    When I have added multiple products using the quick view functionality to the cart
+    Then I can process to payment by pressing the checkout button*/
+    @Test
+    public void goToWomanHomePage(){
+        navigationPages.womensPage().goToWomensPage();
+    }
+
+    @Test
+    public void addMultipleItemInCart(){
+        automationPracticeSite.getBasket().processToCheckout();
     }
 }
--- src/test/java/com/spartaglobal/eng43project/automationPracticeSite/Pages/Basket.java
+++ src/test/java/com/spartaglobal/eng43project/automationPracticeSite/Pages/Basket.java
@@ -1,16 +1,64 @@
 package com.spartaglobal.eng43project.automationPracticeSite.Pages;
 
+import com.spartaglobal.eng43project.automationPracticeSite.Pages.Navigation.NavigationPages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Basket {
     private WebDriver driver;
+    private NavigationPages navigationPages;
     private String basketUrl = "http://automationpractice.com/index.php?controller=order";
+    private By cloth2AddToBasketID = By.cssSelector("#center_column > ul > li:nth-child(2) > div > div.right-block > div.button-container > a.button.ajax_add_to_cart_button.btn.btn-default");
+    private By cloth3AddToBasketID = By.cssSelector("#center_column > ul > li.ajax_block_product.col-xs-12.col-sm-6.col-md-4.last-in-line.first-item-of-tablet-line.last-item-of-mobile-line > div > div.right-block > div.button-container > a.button.ajax_add_to_cart_button.btn.btn-default");
+    private By checkoutButtonID = By.linkText("Proceed to checkout");
+    private By continueToShoppingID = By.cssSelector("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > span");
+
     public Basket(WebDriver driver) {
         this.driver = driver;
+        navigationPages = new NavigationPages(driver);
     }
+
 
     public Basket goToBasketURL(){
         driver.navigate().to(basketUrl);
         return this;
     }
+
+    public Basket goToWomanPage(){
+        navigationPages.womensPage().goToWomensPage();
+        return this;
+    }
+
+    public Basket selectProduct1ToCart(){
+        goToWomanPage();
+        driver.findElement(cloth3AddToBasketID).click();
+        return this;
+    }
+
+    public Basket clickShoppingButton(){
+        selectProduct1ToCart().waitingTime();
+        driver.findElement(continueToShoppingID).click();
+        return this;
+    }
+
+    public Basket selectProduct2ToCart(){
+        clickShoppingButton();
+        driver.findElement(cloth2AddToBasketID).click();
+        return this;
+    }
+
+    public Basket waitingTime(){
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return this;
+    }
+
+    public Basket processToCheckout(){
+        selectProduct2ToCart().waitingTime();
+        driver.findElement(checkoutButtonID).click();
+        return this;
+    }
+
 }

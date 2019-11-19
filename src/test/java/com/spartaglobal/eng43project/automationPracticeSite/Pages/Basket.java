4752e2f6e395caa5bb73baa7ed075e6fee2f4a0e
Dana Korang-Awua <danaawua@hotmail.co.uk>
2019-11-19 10:56:21 +0000

    sample commit

--- src/test/java/com/spartaglobal/eng43project/automationPracticeSite/Pages/Basket.java
+++ src/test/java/com/spartaglobal/eng43project/automationPracticeSite/Pages/Basket.java
@@ -5,12 +5,14 @@ import org.openqa.selenium.WebDriver;
 public class Basket {
     private WebDriver driver;
     private String basketUrl = "http://automationpractice.com/index.php?controller=order";
+
     public Basket(WebDriver driver) {
         this.driver = driver;
     }
 
-    public Basket goToBasketURL(){
+    public Basket goToBasketURL() {
         driver.navigate().to(basketUrl);
         return this;
     }
+
 }

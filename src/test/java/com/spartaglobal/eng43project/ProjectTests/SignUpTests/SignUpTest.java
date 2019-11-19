edc70e6b1007946e1a58faa29127dcb402016e40
Sham302 <55741747+Sham302@users.noreply.github.com>
2019-11-19 10:09:18 +0000

    Sign Up code added

--- src/test/java/com/spartaglobal/eng43project/ProjectTests/SignUpTests/SignUpTest.java
+++ src/test/java/com/spartaglobal/eng43project/ProjectTests/SignUpTests/SignUpTest.java
@@ -1,7 +1,50 @@
 package com.spartaglobal.eng43project.ProjectTests.SignUpTests;
 
+import com.spartaglobal.eng43project.SeleniumConfig.SeleniumConfig;
+import com.spartaglobal.eng43project.automationPracticeSite.AutomationPracticeSite;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Iterator;
+
 /**
  * A base example of how to lay this class out can be seen in BasketTest.java
  **/
 public class SignUpTest {
+    private static ChromeDriver driver;
+    private static AutomationPracticeSite site;
+
+    private String accountExistsError = "An account using this email address has already been registered. Please enter a valid password or request a new one.";
+
+
+    @Before
+    public void preRecs(){
+        SeleniumConfig chromeDriverConfig = new SeleniumConfig("chrome","C:\\Users\\Shmaila Rehman\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+        site = new AutomationPracticeSite(chromeDriverConfig.getDriver());
+    }
+
+//    @AfterClass
+//    public static void teardown(){
+//     site.closeDriver();
+//    }
+
+    @Test
+    public void emailAccountExists() {
+
+        Iterator<WebElement> itr = site.getSignup().goToSignUpURL().inputEmail("srehman@spartaglobal.com").clickCreateAccountButton().getEmailExistsError().iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next().getText());
+        }
+    }
+
+    @Test
+    public void signUpSuccessful (){
+        site.getSignup().goToSignUpURL().inputEmail("jdjfi@gmail.com").clickCreateAccountButton();
+    }
+
+
 }

a922a1e5b952bc31950e0ab57c0471c74e005489
IYotova <iliyanayotova@bennington.edu>
2019-11-19 15:39:48 +0000

    commonpage properties class, tests, clean subpages

--- src/test/java/com/spartaglobal/eng43project/ProjectTests/PageNavigationTests/PageNavigationTest.java
+++ src/test/java/com/spartaglobal/eng43project/ProjectTests/PageNavigationTests/PageNavigationTest.java
@@ -2,6 +2,7 @@ package com.spartaglobal.eng43project.ProjectTests.PageNavigationTests;
 
 import com.spartaglobal.eng43project.SeleniumConfig.SeleniumConfig;
 import com.spartaglobal.eng43project.automationPracticeSite.AutomationPracticeSite;
+import com.spartaglobal.eng43project.automationPracticeSite.Pages.Navigation.CommonPageProperties;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -16,12 +17,14 @@ public class PageNavigationTest {
 
     private static SeleniumConfig seleniumConfig;
     private static AutomationPracticeSite automationPracticeSite;
+    private static CommonPageProperties commonPageProperties;
 
     @BeforeClass
     public static void setup(){
         // TODO: 18/11/2019 Change the driverPath to your own path!
         seleniumConfig = new SeleniumConfig("chrome","C:\\Users\\IYotova\\Downloads\\chromedriver_win32\\chromedriver.exe");
         automationPracticeSite = new AutomationPracticeSite(seleniumConfig.getDriver());
+        commonPageProperties = new CommonPageProperties(seleniumConfig.getDriver());
     }
 
     @AfterClass
@@ -37,9 +40,57 @@ public class PageNavigationTest {
 
     @Test
     public void shouldClickWomenLink() throws Exception{
-        automationPracticeSite.getNavigationPages().homePage().goToHomePageURL().getNavToWomen();
+        automationPracticeSite.getNavigationPages().homePage().goToHomePageURL();
+        commonPageProperties.clickWomenInNavBar();
         Assert.assertEquals("http://automationpractice.com/index.php?id_category=3&controller=category",seleniumConfig.getDriver().getCurrentUrl());
     }
 
+    @Test
+    public void shouldClickTshirtsLink() throws Exception{
+        automationPracticeSite.getNavigationPages().homePage().goToHomePageURL();
+        commonPageProperties.clickWomenHeaderTopsTshirtLink();
+        Assert.assertEquals("http://automationpractice.com/index.php?id_category=5&controller=category",seleniumConfig.getDriver().getCurrentUrl());
+    }
 
+    @Test
+    public void shouldClickBlousesLink() throws Exception{
+        automationPracticeSite.getNavigationPages().homePage().goToHomePageURL();
+        commonPageProperties.clickWomenHeaderTopsBlousesLink();
+        Assert.assertEquals("http://automationpractice.com/index.php?id_category=7&controller=category",seleniumConfig.getDriver().getCurrentUrl());
+    }
+
+    @Test
+    public void shouldClickDressesLink() throws Exception{
+        automationPracticeSite.getNavigationPages().homePage().goToHomePageURL();
+        commonPageProperties.clickDressesHeader();
+        Assert.assertEquals("http://automationpractice.com/index.php?id_category=8&controller=category",seleniumConfig.getDriver().getCurrentUrl());
+    }
+
+    @Test
+    public void shouldClickDressesCasualDressLink() throws Exception{
+        automationPracticeSite.getNavigationPages().homePage().goToHomePageURL();
+        commonPageProperties.clickDressesHeaderCasualDresses();
+        Assert.assertEquals("http://automationpractice.com/index.php?id_category=9&controller=category",seleniumConfig.getDriver().getCurrentUrl());
+    }
+
+    @Test
+    public void shouldClickDressesEveningDressLink() throws Exception{
+        automationPracticeSite.getNavigationPages().homePage().goToHomePageURL();
+        commonPageProperties.clickDressesHeaderEveningDresses();
+        Assert.assertEquals("http://automationpractice.com/index.php?id_category=10&controller=category",seleniumConfig.getDriver().getCurrentUrl());
+    }
+
+    @Test
+    public void shouldClickDressesSummerDressLink() throws Exception{
+        automationPracticeSite.getNavigationPages().homePage().goToHomePageURL();
+        commonPageProperties.clickDressesHeaderSummerDresses();
+        Assert.assertEquals("http://automationpractice.com/index.php?id_category=11&controller=category",seleniumConfig.getDriver().getCurrentUrl());
+    }
+
+    @Test
+    public void shouldClickTshirtLink() throws Exception{
+        automationPracticeSite.getNavigationPages().homePage().goToHomePageURL();
+        commonPageProperties.clickTshirtHeader();
+        Assert.assertEquals("http://automationpractice.com/index.php?id_category=5&controller=category",seleniumConfig.getDriver().getCurrentUrl());
+    }
 }

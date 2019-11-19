21be78568968af7e56bfef231eb6d0695bf580eb
Edward James <ejames@spartaglobal.com>
2019-11-19 10:21:30 +0000

    Added Proceed to Checkout method

--- src/test/java/com/spartaglobal/eng43project/automationPracticeSite/Pages/Basket.java
+++ src/test/java/com/spartaglobal/eng43project/automationPracticeSite/Pages/Basket.java
@@ -8,7 +8,10 @@ public class Basket {
     private WebDriver driver;
     private String basketUrl = "http://automationpractice.com/index.php?controller=order";
     private NavigationPages navigationPages;
-    private By printedDressQuickBuy=By.cssSelector("#homefeatured > li.ajax_block_product.col-xs-12.col-sm-4.col-md-3.last-item-of-tablet-line.first-item-of-mobile-line > div > div.right-block > div.button-container > a.button.ajax_add_to_cart_button.btn.btn-default > span");
+    private By printedDressQuickBuy = By.cssSelector("#homefeatured > li.ajax_block_product.col-xs-12.col-sm-4.col-md-3.last-item-of-tablet-line.first-item-of-mobile-line > div > div.right-block > div.button-container > a.button.ajax_add_to_cart_button.btn.btn-default > span");
+    private By proceedToBasketButton = By.cssSelector("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a > span");
+    private By proceedToCheckoutButton = By.cssSelector("#center_column > p.cart_navigation.clearfix > a.button.btn.btn-default.standard-checkout.button-medium > span");
+
     public Basket(WebDriver driver) {
         this.driver = driver;
         navigationPages = new NavigationPages(driver);
@@ -22,7 +25,13 @@ public class Basket {
     public Basket addItemToBasket(){
         navigationPages.homePage().goToHomePageURL();
         driver.findElement(printedDressQuickBuy).click();
-        driver.navigate().to(basketUrl);
+        driver.findElement(proceedToBasketButton).click();
         return this;
     }
+
+    public Basket proceedToCheckout(){
+        driver.navigate().to(basketUrl);
+        driver.findElement(proceedToCheckoutButton).click();
+    }
+
 }

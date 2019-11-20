cd17f314089dd50300f52474b812acdc0341912c
Anis Subba <21302329@student.uwl.ac.uk>
2019-11-20 13:01:42 +0000

    refactored the code in add button and subtract button

--- src/test/java/com/spartaglobal/eng43project/automationPracticeSite/Pages/Basket.java
+++ src/test/java/com/spartaglobal/eng43project/automationPracticeSite/Pages/Basket.java
@@ -1,17 +1,10 @@
 package com.spartaglobal.eng43project.automationPracticeSite.Pages;
 
 import com.spartaglobal.eng43project.automationPracticeSite.Pages.Navigation.NavigationPages;
-import io.cucumber.java.bs.A;
-import io.cucumber.java.en_old.Ac;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Basket {
@@ -20,6 +13,8 @@ public class Basket {
     private String basketUrl = "http://automationpractice.com/index.php?controller=order";
 //    private By printedDressQuickBuy = By.cssSelector("#homefeatured > li.ajax_block_product.col-xs-12.col-sm-4.col-md-3.last-item-of-tablet-line.first-item-of-mobile-line > div > div.right-block > div.button-container > a.button.ajax_add_to_cart_button.btn.btn-default > span");
     private By addProduct3ID = By.xpath("//*[@id=\"center_column\"]/ul/li[3]/div/div[2]/div[2]/a[1]");
+    private By summaryProduct3AddButtonID = By.id("cart_quantity_up_3_13_0_0");
+    private By summaryProduct3SubtractButtonID = By.id("cart_quantity_down_3_13_0_0");
 
     public Basket(WebDriver driver) {
         this.driver = driver;
@@ -76,17 +71,16 @@ public class Basket {
             else {
                 clickShoppingButton();
             }
-        }proceedToCheckout();
+        }
+        proceedToCheckout();
         return this;
     }
-
-
 
     public Basket increaseQuantityInSummary(){
 
         addItemToBasket().waitForElement().proceedToCheckout().waitForElement();
         for (int i = 0; i < 11; i++) {
-            driver.findElement(By.id("cart_quantity_up_3_13_0_0")).click();
+            driver.findElement(summaryProduct3AddButtonID).click();
             waitForElement();
         }
         return this;
@@ -99,7 +93,7 @@ public class Basket {
 //            waitForElement();
 //        }
         addItemToBasket().waitForElement().proceedToCheckout().waitForElement();
-        driver.findElement(By.id("cart_quantity_down_3_13_0_0")).click();
+        driver.findElement(summaryProduct3SubtractButtonID).click();
         return this;
     }
 
